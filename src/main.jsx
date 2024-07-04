8f8d99833ad12fc7c9eb0b8fb0dd45ced9fa37e7
Ujjwal Sharma <us36335@gmail.com>
2024-07-04 22:57:35 +0530

    Bearer Token using Interceptors

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { store } from './store.js'
 import { Provider } from 'react-redux'
+import axios from 'axios';
+
+axios.interceptors.request.use((request) => {
+  request.headers.token = 'Bearer Test'
+  return request
+})
+
+axios.interceptors.response.use((response) => {
+  return response
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
